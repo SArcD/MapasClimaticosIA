447f11f6b9fc063f61217eb8142e4467e2220d18
SArcD <141703135+SArcD@users.noreply.github.com>
2025-04-23 23:11:22 -0600

    Update main.py

--- main.py
+++ main.py
@@ -3058,7 +3058,7 @@ elif seccion == "Análisis con Prophet":
     except Exception as e:
         st.error(f"Error en el flujo de procesamiento: {e}")
 
-elif secccion = "Trayectoria Solar":
+elif secccion == "Trayectoria Solar":
         import math
         import numpy as np
         import pandas as pd
